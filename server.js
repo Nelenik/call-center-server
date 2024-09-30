927bbd57aad1b94b396ee8ba289b7933086440c6
NeLeNik <lnek10.1990@gmail.com>
2024-09-30 22:08:09 +0300

    added auth

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import jsonServer from "json-server";
 import path from "path";
 import { fileURLToPath } from "url";
+import { randomUUID } from "crypto";
+import { readFileSync } from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -28,6 +30,64 @@ server.use(
     "/calls/:employeeId": "/calls?employeeId=:employeeId",
   })
 );
+
+const sessions = new Map();
+
+const getDbScript = (property) => {
+  const db = JSON.parse(readFileSync("./db.json", "utf-8"));
+  return db[property];
+};
+
+const getUserByUserName = (username) => {
+  const users = getDbScript("users");
+  return users.find((el) => el.username === username);
+};
+const closeSession = (token) => {
+  sessions.delete(token);
+};
+
+const openSession = (token, userId) => {
+  sessions.set(token, userId);
+  const sessionExpiryTime = 30 * 60 * 1000;
+  setTimeout(closeSession, sessionExpiryTime);
+};
+
+const isAuthorized = (req) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  return token && sessions.has(token);
+};
+
+server.post("/auth/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = getUserByUserName(username.trim());
+  if (!user || user.password !== password) {
+    return res.status(401).send("Invalid username or password");
+  }
+  const token = randomUUID();
+  openSession(token, user.id);
+
+  res
+    .status(200)
+    .json({ accesToken: token, employeeId: user.employeeId, role: user.role });
+});
+
+server.post("/auth/logout", (req, res) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (token && sessions.has(token)) {
+    closeSession(token);
+    return res.status(200).json({ message: "Logged out successfully" });
+  }
+  return res.status(404).json("Session not found");
+});
+
+server.use((req, res, next) => {
+  if (isAuthorized(req)) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+});
+
 server.use(router);
 server.listen(4000, () => {
   console.log("JSON Server is running on http://localhost:4000");
