d29ba3d170b724eb7278701081307c7b02147a11
NeLeNik <lnek10.1990@gmail.com>
2024-09-30 08:52:38 +0300

    added fake server files

---
+++ server.js
@@ -0,0 +1,33 @@
+import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  const newDate = new Date().toISOString();
+  if (req.method === "POST" || req.method === "PUT") {
+    req.body.createdAt = newDate;
+    req.body.updatedAt = newDate;
+  } else if (req.method === "PATCH") {
+    req.body.updatedAt = newDate;
+  }
+  next();
+});
+server.use(
+  jsonServer.rewriter({
+    "/calls/:employeeId": "/calls?employeeId=:employeeId",
+  })
+);
+server.use(router);
+server.listen(4000, () => {
+  console.log("JSON Server is running on http://localhost:4000");
+});
